b380f7757ef0160b4f4e944e6c377f07b9f5ac57
Suleyman Musayev <slmusayev@gmail.com>
2022-07-14 22:32:13 +0500

    change routes and view for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'homepage/index'
+  root to: 'homepage#index', as: 'homepage'
 
-  resources :tenants
   resources :flats
   resources :users
+  resources :tenants, only: %i[create destroy]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'homepage#index', as: 'homepage'
 end
