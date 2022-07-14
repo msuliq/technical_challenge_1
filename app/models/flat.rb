2d585d3ed00a9b394a52075239e5ad19193559e3
Suleyman Musayev <slmusayev@gmail.com>
2022-07-14 22:30:13 +0500

    add changes to flat and tenants models

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,8 +1,7 @@
 class Flat < ApplicationRecord
     has_many :tenants, dependent: :destroy
 
-
-    def add_user(user_id)
+    def add_tenant(user_id)
         current_tenant = tenants.find_by(user_id: user_id)
         if current_tenant
             current_tenant.quantity += 1
@@ -11,8 +10,14 @@ class Flat < ApplicationRecord
         end
             current_tenant
     end
-    
-    def total_tenants
-        tenants.to_a.sum { |tenant| tenant.total_tenants }
+
+    def remove_tenant(user_id)
+        current_tenant = tenants.find_by(user_id: user_id)
+        if current_tenant > 2
+            current_tenant.quantity -= 1
+        else
+            current_tenant = tenants.destroy(user_id: user_id)
+        end
     end
+
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,9 +1,5 @@
 class Tenant < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :flat
-  validates :flat_id, presence: true
 
-  def total_tenants
-    user.quantity
-  end
 end
