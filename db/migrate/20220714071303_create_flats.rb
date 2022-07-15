8f5472db57483ed490d0fc9a3bdaa8b764ba02d3
Suleyman Musayev <slmusayev@gmail.com>
2022-07-15 17:13:50 +0500

    add changes to create flats migration file

--- db/migrate/20220714071303_create_flats.rb
+++ db/migrate/20220714071303_create_flats.rb
@@ -2,8 +2,8 @@ class CreateFlats < ActiveRecord::Migration[7.0]
   def change
     create_table :flats do |t|
       t.string :name
-      t.decimal :latitude, :precision => 15, :scale => 10, :default => 0.0
-      t.decimal :longitude, :precision => 15, :scale => 10, :default => 0.0
+      t.float :latitude, :precision => 6, :scale => 10, null: false
+      t.float :longitude, :precision => 6, :scale => 10, null: false
 
       t.timestamps
     end
